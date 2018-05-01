7196f13de44ba3d6abbf49cbbf4b608f9dd670f5
Matt Tucker <mtuck06@gmail.com>
2018-05-01 14:58:28 -0500

    added fun.js

---
+++ caleb_dekker/fun.js
@@ -0,0 +1,17 @@
+// Ints, String - Primitive Data Types
+// [], {} - Non-Primitive
+
+// var // ES5
+// // Global Scope
+
+// let // ES6
+// // Block Scope
+var arr = [1, 2, 3];
+
+for (var x in arr) {
+	console.log(arr[x]);
+}
+
+
+
+
